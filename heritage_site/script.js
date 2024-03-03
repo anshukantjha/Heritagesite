945ed25b2f629820c3694f81f6f387861aba9b52
anshukantjha <149821359+anshukantjha@users.noreply.github.com>
2024-03-03 23:14:43 +0530

    Add files via upload
    Made a few changes in the location section and used location icon

--- heritage_site/script.js
+++ heritage_site/script.js
@@ -61,8 +61,6 @@ if (styleElement.styleSheet) {
 }
 document.head.appendChild(styleElement);
 
-
-
 const cardData = [
     // CULTURAL
     // Ajanta Cave
@@ -72,7 +70,6 @@ const cardData = [
         description: "The first Buddhist cave monuments at Ajanta date from the 2nd and 1st centuries B.C. During the Gupta period.",
         location: "Maharastra",
         century: "1st B.C",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=tom?size=64",
         tags: "Caves"
 
 
@@ -80,11 +77,10 @@ const cardData = [
     // Nalanda Mahavihara
     {
         title: "Nalanda Mahavihara",
-        imageUrl: "contents/images/nalanda_mahavihara.png",
+        imageUrl: "images/nalanda_mahavihara.png",
         description: "Nalanda Mahavihara was a large Buddhist monastery and an important center of learning in ancient India. It flourished . ",
         location: "Bihar",
         century: "10th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=nalanda?size=64",
         tags: "University"
 
     },
@@ -95,7 +91,6 @@ const cardData = [
         description: "The Buddhist monuments at Sanchi are a group of stupas, monasteries, temples, and pillars located in the town of Sanchi, Madhya Pradesh.",
         location: "Madhya Pradesh",
         century: "3rd BCE",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=sanchi?size=64",
         tags: "Religious"
 
     },
@@ -106,7 +101,6 @@ const cardData = [
         description: "Champaner-Pavagadh Archaeological Park is a UNESCO World Heritage Site located in Gujarat, India.",
         location: "Gujarat",
         century: "8th - 14th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=champaner?size=64",
         tags: "Archae"
 
     },
@@ -117,7 +111,6 @@ const cardData = [
         description: "Chhatrapati Shivaji Terminus (CST), formerly known as Victoria Terminus (VT), is a historic railway station .",
         location: " Maharashtra",
         century: "19th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=cst?size=64",
         tags: " Railway "
 
     },
@@ -128,7 +121,6 @@ const cardData = [
         description: "The Churches and Convents of Goa are a group of Catholic religious buildings located in the former Portuguese colony of Goa",
         location: "Goa",
         century: "17th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=goachurches?size=64",
         tags: "Religious"
 
     },
@@ -136,10 +128,9 @@ const cardData = [
     {
         title: "Dholavira: a Harappan City",
         imageUrl: "https://source.unsplash.com/random/600×400?dholavira",
-        description: "Dholavira is an archaeological site belonging to the ancient Indus Valley Civilization, also known as the Harappan Civilization.",
+        description: "Dholavira is an archaeological site belonging to the ancient Indus Valley Civilization, also known as the Harappan.",
         location: "Kutch, Gujarat",
         century: "3rd BCE",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=dholavira?size=64",
         tags: "Archae"
 
     },
@@ -150,7 +141,6 @@ const cardData = [
         description: "The Elephanta Caves  located on Elephanta Island in Mumbai.where Lord Shiva is worshipped",
         location: " Maharashtra",
         century: "5th - 6th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=elephantacaves?size=64",
         tags: "Caves"
 
     },
@@ -161,7 +151,6 @@ const cardData = [
         description: "The Ellora Caves represent a remarkable example of Indian rock-cut architecture. located in maharasthra",
         location: "Maharashtra",
         century: "8th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=elloracaves?size=64",
         tags: "Cave "
 
     },
@@ -172,7 +161,6 @@ const cardData = [
         description: "Mughal architectural complex in Uttar Pradesh, India. Constructed in the 16th century by Akbar.",
         location: "Uttar Pradesh",
         century: "16th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=fatehpursikri?size=64",
         tags: "Archae"
 
     },
@@ -183,7 +171,6 @@ const cardData = [
         description: "Temples in Tamil Nadu, showcasing Chola architecture from 11th C.E",
         location: "Tamil Nadu",
         century: "11th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=cholatemples?size=64",
         tags: "Temple"
     },
     {
@@ -192,7 +179,6 @@ const cardData = [
         description: "Capital of Rajasthan, known as the 'Pink City' for its pink-hued buildings and historic splendor.",
         location: "Rajasthan",
         century: "18th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=jaipur?size=64",
         tags: "Capital"
     },
     {
@@ -201,7 +187,6 @@ const cardData = [
         description: "An astronomical observatory built by Maharaja Jai Singh II in the 18th century.",
         location: "Rajasthan",
         century: "18th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=jantarmantar?size=64",
         tags: " Astronomy"
     },
     {
@@ -210,7 +195,6 @@ const cardData = [
         description: "A UNESCO World Heritage Site in Odisha, India, known for its stunning architecture. located in Konark",
         location: "Odisha",
         century: "13th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=suntemplekonarak?size=64",
         tags: "Temple"
     },
     {
@@ -219,7 +203,6 @@ const cardData = [
         description: "A UNESCO World Heritage Site in Agra, India, known as a symbol of love and beauty.",
         location: "U.P",
         century: "17th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=tajmahal?size=64",
         tags: "Monument"
     }
 
@@ -241,7 +224,6 @@ const cardDataNatural = [
         description: "A UNESCO World Heritage Site in Himachal Pradesh, India, known for its biodiversity and stunning landscapes.",
         location: "Himachal",
         century: "20th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=himalayanpark?size=64",
         tags: "Park"
     },
     {
@@ -250,7 +232,6 @@ const cardDataNatural = [
         description: "A UNESCO World Heritage Site in Rajasthan, India, renowned for its bird sanctuary. which is National Park",
         location: "Rajasthan",
         century: "20th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=keoladeonationalpark?size=64",
         tags: "Sanctuary"
     },
     {
@@ -259,7 +240,6 @@ const cardDataNatural = [
         description: "A UNESCO World Heritage Site in Assam, India, known for its rich biodiversity and conservation efforts. for the Safety of Animals",
         location: "Assam",
         century: "20th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=manaswildlifesanctuary?size=64",
         tags: "Sanctuary"
     },
     {
@@ -268,7 +248,6 @@ const cardDataNatural = [
         description: "UNESCO World Heritage Sites in Uttarakhand, India, renowned for their natural beauty and biodiversity.",
         location: "Uttarakhand",
         century: "20th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=nandadevi-valleyofflowers?size=64",
         tags: "Park"
     },
     {
@@ -277,7 +256,6 @@ const cardDataNatural = [
         description: "A UNESCO World Heritage Site in West Bengal, India, known for its mangrove forests and Royal Bengal tigers.",
         location: "West Bengal",
         century: "20th C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=sundarbansnationalpark?size=64",
         tags: "Forest"
     },
     {
@@ -286,7 +264,6 @@ const cardDataNatural = [
         description: "UNESCO World Heritage Site in India, renowned for rich biodiversity and unique ecosystems.It has Immense biodiversity with all kinds of animals and Plants",
         location: "India",
         century: "21st C.E",
-        avatarImage: "https://api.dicebear.com/7.x/notionists/svg?seed=westernghats?size=64",
         tags: "Biodiversity"
     }
 ]
@@ -302,7 +279,7 @@ cardData.forEach((data, index) => {
 
     const anchors = clonedCardContainer.getElementsByTagName('a');
     for (let i = 0; i < anchors.length; i++) {
-        anchors[i].href = `contents/${data.title}.html`;
+        anchors[i].href = `${data.title}.html`;
     }
 
     const titleElement = clonedCardContainer.querySelector('.card_title');
@@ -311,14 +288,13 @@ cardData.forEach((data, index) => {
     const descElement = clonedCardContainer.querySelector('.card_desc');
     descElement.textContent = data.description;
 
-    const authorStateElement = clonedCardContainer.querySelector('.author_info span:first-child');
-    authorStateElement.textContent = data.location;
-    const authorDateElement = clonedCardContainer.querySelector('.author_info span:last-child');
-    authorDateElement.textContent = data.century;
+    const locationStateElement = clonedCardContainer.querySelector('.location_info span:first-child');
+    locationStateElement.textContent = data.location;
+    const locationDateElement = clonedCardContainer.querySelector('.location_info span:last-child');
+    locationDateElement.textContent = data.century;
 
-    const avatarElement = clonedCardContainer.querySelector('.author_avatar img');
-    avatarElement.src = data.avatarImage;
-    avatarElement.alt = 'Avatar';
+    const locationElement = clonedCardContainer.querySelector('.location');
+    locationElement.HTML=`<i class="fa-solid fa-location-dot"></i> `;
 
     const tagElement = clonedCardContainer.querySelector('.tag_container span');
     tagElement.textContent = data.tags;
@@ -342,14 +318,13 @@ cardDataNatural.forEach((data, index) => {
     const descElement = clonedCardContainer.querySelector('.card_desc');
     descElement.textContent = data.description;
 
-    const authorStateElement = clonedCardContainer.querySelector('.author_info span:first-child');
+    const authorStateElement = clonedCardContainer.querySelector('.location_info span:first-child');
     authorStateElement.textContent = data.location;
-    const authorDateElement = clonedCardContainer.querySelector('.author_info span:last-child');
+    const authorDateElement = clonedCardContainer.querySelector('.location_info span:last-child');
     authorDateElement.textContent = data.century;
 
-    const avatarElement = clonedCardContainer.querySelector('.author_avatar img');
-    avatarElement.src = data.avatarImage;
-    avatarElement.alt = 'Avatar';
+    const locationElement = clonedCardContainer.querySelector('.location');
+    locationElement.HTML=`<i class="fa-solid fa-location-dot"></i> `;
 
     const tagElement = clonedCardContainer.querySelector('.tag_container span');
     tagElement.textContent = data.tags;
